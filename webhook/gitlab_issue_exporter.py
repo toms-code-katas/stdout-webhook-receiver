43c1f31215ee9f099c8370cbd64377b15e91ede9
tom1299 <tom1299@jeeatwork.com>
2022-10-04 15:23:10 +0200

    Made first example work

--- webhook/gitlab_issue_exporter.py
+++ webhook/gitlab_issue_exporter.py
@@ -1,6 +1,9 @@
 """
 A handler for handling post requests and opening gitlab issues
 """
+import gitlab
+import os
+import socketserver
 from http.server import BaseHTTPRequestHandler
 from http.server import HTTPServer
 import json
@@ -21,6 +24,16 @@ class AlarmHandler(BaseHTTPRequestHandler):
     A handler for handling post requests and opening gitlab issues
     """
 
+    def __init__(self, request: bytes, client_address: tuple[str, int], server: socketserver.BaseServer) -> None:
+        self.gitlab_url = os.getenv("GITLAB_URL", "https://gitlab.com")
+        self.gitlab_project_id = os.getenv("GITLAB_PROJECT_ID", "38561817")
+        if os.path.isfile("/etc/gitlab-issue-exporter/token"):
+            with open('/etc/gitlab-issue-exporter/token', 'r') as secret_file:
+                self.gitlab_token = secret_file.read().rstrip()
+        else:
+            self.gitlab_token = os.getenv("GITLAB_TOKEN")
+        super().__init__(request, client_address, server)
+
     # pylint: disable=C0103
     def do_POST(self):
         """
@@ -36,8 +49,18 @@ class AlarmHandler(BaseHTTPRequestHandler):
         else:
             logger.debug("Received alert sate is firing. Start processing")
 
+            gl = gitlab.Gitlab(self.gitlab_url, private_token=self.gitlab_token)
+            project = gl.projects.get(self.gitlab_project_id, lazy=True)
+
+            environment = data["commonLabels"]["platform_environment"]
+            deployment = data["commonLabels"]["deployment"]
+
+            for issue in project.search('issues', [deployment, environment], as_list=False):
+                print(issue)
+
         self.send_response(200)
         self.end_headers()
+
 
 def main():
     httpd = HTTPServer(('', 8080), AlarmHandler)
