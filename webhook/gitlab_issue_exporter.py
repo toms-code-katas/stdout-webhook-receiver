70abf7cb3fcbb5d746b286d7ddbf1c8148ee841b
tom1299 <tom1299@jeeatwork.com>
2022-10-05 09:57:57 +0200

    Some refactoring

--- webhook/gitlab_issue_exporter.py
+++ webhook/gitlab_issue_exporter.py
@@ -1,6 +1,7 @@
 """
 A handler for handling post requests and opening gitlab issues
 """
+import datetime
 import gitlab
 import os
 import socketserver
@@ -28,6 +29,7 @@ class AlarmHandler(BaseHTTPRequestHandler):
         self.gitlab_url = os.getenv("GITLAB_URL", "https://gitlab.com")
         self.gitlab_project_id = os.getenv("GITLAB_PROJECT_ID", "38561817")
         self.gitlab_username = os.getenv("GITLAB_USERNAME", "gitlab-issue-exporter")
+        self.grace_period = os.getenv("GRACE_PERIOD", 600)
         if os.path.isfile("/etc/gitlab-issue-exporter/token"):
             with open('/etc/gitlab-issue-exporter/token', 'r') as secret_file:
                 self.gitlab_token = secret_file.read().rstrip()
@@ -56,25 +58,49 @@ class AlarmHandler(BaseHTTPRequestHandler):
             environment = data["commonLabels"]["platform_environment"]
             deployment = data["commonLabels"]["deployment"]
 
-            for issue in project.search('issues', [deployment, environment], as_list=False):
-                if issue["state"] == "opened":
-                    issue_id = issue['iid']
-                    logger.debug(f"Found already existing issue issue \"{issue_id}\", \"{issue['title']}\"")
-                    already_created_notes = 0
-                    last_note_date = None
-                    full_issue = project.issues.get(issue_id)
-                    for note in full_issue.notes.list():
-                        if self.gitlab_username == note.author["username"]:
-                            already_created_notes += 1
-                    logger.debug(f"Found {already_created_notes} notes created by {self.gitlab_username}")
-                    if already_created_notes <= 9:
-                        full_issue.notes.create({'body': 'created by issue exporter'})
-                    elif already_created_notes > 10:
-                        logger.debug("Maximum number of notes reached for issue. Omit creating new ones")
+            related_issue = self.find_related_issue(deployment, environment, project)
+            if not related_issue:
+                pass
+            else:
+                issue_id = related_issue["iid"]
+                already_created_notes = 0
+                latest_creation_date = None
+                full_issue = project.issues.get(issue_id)
+                for note in full_issue.notes.list():
+                    if self.gitlab_username == note.author["username"]:
+                        already_created_notes += 1
+                        creation_date = datetime.datetime.strptime(note.created_at, "%Y-%m-%dT%H:%M:%S.%fZ")
+                        if not latest_creation_date:
+                            latest_creation_date = creation_date
+                        elif creation_date > latest_creation_date:
+                            latest_creation_date = creation_date
+                logger.debug(f"Found {already_created_notes} notes created by {self.gitlab_username}")
+                if already_created_notes <= 9 and self.has_grace_period_elapsed(latest_creation_date):
+                    full_issue.notes.create({'body': 'created by issue exporter'})
+                elif already_created_notes > 10:
+                    logger.debug("Maximum number of notes reached for issue. Omit creating new ones")
 
         self.send_response(200)
         self.end_headers()
 
+    def find_related_issue(self, deployment, environment, project):
+        for issue in project.search('issues', [deployment, environment], as_list=False, order_by="created_at"):
+            if issue["state"] == "opened":
+                issue_id = issue['iid']
+                logger.debug(f"Found already existing issue issue \"{issue_id}\", \"{issue['title']}\"")
+                break
+        return issue
+
+    def has_grace_period_elapsed(self, latest_creation_date):
+        if not latest_creation_date:
+            return True
+        seconds_since_last_note = int((datetime.datetime.utcnow() - latest_creation_date).total_seconds())
+        if seconds_since_last_note < self.grace_period:
+            logger.debug(
+                f"Seconds since last note creation {seconds_since_last_note} than smaller {self.grace_period}."
+                f" Omit creation of note")
+            return False
+        return True
 
 def main():
     httpd = HTTPServer(('', 8080), AlarmHandler)
